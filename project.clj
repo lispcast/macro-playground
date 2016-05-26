c4578fd9fd84754ceaa097710d78d13835bf1e9e
Eric Normand <ericwnormand@gmail.com>
2016-05-26 13:07:08 -0500

    Improved description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject macro-playground "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Macro implementations for learning how macros work"
   :url "https://purelyfunctional.tv/clojure-macros"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
